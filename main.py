5a796cc3c39009117e55940f73b5f0d8db5d4114
Pedro Ferracini <pedro.ferracini@imusics.com>
2020-05-24 21:19:37 -0300

    Removes data from parameters, print population and parameters

--- main.py
+++ main.py
@@ -2,20 +2,16 @@ from algoritmoGenetico import AlgoritmoGenetico
 from hdbscancluster import HDBScan
 
 import warnings
-import pandas as pd
 import numpy as np
 
 def main():
-  entrada = pd.read_csv('../7483_patients.csv', header=None)
-
   parametros = {
-    'algorithm' : ['best', 'generic', 'prims_kdtree', 'prims_balltree', 'boruvka_kdtree', 'boruvka_balltree'],
-    'min_cluster_size' : [2<<exponent for exponent in range(7)],
-    'min_samples': [1<<exponent for exponent in range(6)],
-    'cluster_selection_method' : ['eom', 'leaf'],
-    'cluster_selection_epsilon' : np.around(np.arange(0.1, 0.7, 0.05), decimals=2).astype(type('float', (float,), {})),
-    'metric': ['euclidean', 'manhattan', 'jaccard'],
-    'entrada': entrada
+    "algorithm" : ["best", "generic", "prims_kdtree", "prims_balltree", "boruvka_kdtree", "boruvka_balltree"],
+    "min_cluster_size" : [2<<exponent for exponent in range(7)],
+    "min_samples": [1<<exponent for exponent in range(6)],
+    "cluster_selection_method" : ["eom", "leaf"],
+    "cluster_selection_epsilon" : np.around(np.arange(0.1, 0.7, 0.05), decimals=2).astype(type("float", (float,), {})),
+    "metric": ["euclidean", "manhattan", "jaccard"],
   }
 
   #criação do algoritmo genético e da população inicial
@@ -25,15 +21,17 @@ def main():
   populacao = algoritmo.criaPopulacao(tamanho_populacao)
 
   for i in range(0, num_geracoes):
-    print('Geração: ' + str(i+1))
-    print('Fitness: ' + str(algoritmo.mediaPopulacao(populacao)))
-
-    for pop in populacao:
-      pop.printSelf()
-      print('---------\n')
-
+    print("Geração: " + str(i+1))
+    print("Fitness: " + str(algoritmo.mediaPopulacao(populacao)))
+    populacao[0].printSelf()
     populacao = algoritmo.evoluiGeracao(populacao)
+    print('---------\n')
 
-if __name__ == '__main__':
-  warnings.simplefilter('ignore', UserWarning)
+  for i in range(0, len(populacao)):
+    print('Printando a %d populacao: ' % i)
+    print(populacao[i].fitness())
+    populacao[i].printSelf()
+
+if __name__ == "__main__":
+  warnings.simplefilter("ignore", UserWarning)
   main()
